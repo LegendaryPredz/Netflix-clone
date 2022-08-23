69d394dff330089e179f020bf46026c1d0b6f008
Orion Watson <orionwatson27@yahoo.com>
2022-08-23 04:22:31 -0400

    Removed scrolling when modal is open

--- pages/index.tsx
+++ pages/index.tsx
@@ -33,12 +33,16 @@ const Home = ({
   trendingNow,
 }: Props) => {
   const { logout, loading } = useAuth();
-  const showModal = useRecoilValue(modalState)
+  const showModal = useRecoilValue(modalState);
 
   if (loading) return null;
 
   return (
-    <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
+    <div
+      className="`relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh] ${
+        showModal && '!h-screen overflow-hidden'
+      }`"
+    >
       <Head>
         <title>Home - Netflix</title>
         <link rel="icon" href="/favicon.ico" />
@@ -57,7 +61,7 @@ const Home = ({
           <Row title="Documentaries" movies={documentaries} />
         </section>
       </main>
-        {showModal && <Modal />}
+      {showModal && <Modal />}
     </div>
   );
 };
